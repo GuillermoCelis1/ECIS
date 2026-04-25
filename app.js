733af34e24b0ea90a30ad12735c00981a79a35ff
GuillermoCelis1 <guillermo.celis92@gmail.com>
2026-04-25 11:21:30 -0500

    tercer commit

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function weekToEndDate(week, month, year) {
 
 // Clase para manejar la comunicación con la API REST
 class APIManager {
-    constructor(baseURL = 'https://ecis.onrender.com') {
+    constructor(baseURL = 'https://ecis.onrender.com/api') {
         this.baseURL = baseURL;
     }
 
